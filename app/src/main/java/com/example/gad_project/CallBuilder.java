2369e9567b54cf21e8d26814030e0bee22744121
davadeola <adeoladavid15@gmail.com>
2020-09-10 18:03:09 +0300

    Added a toolbar and create the builder for the Retrofit calls

---
+++ app/src/main/java/com/example/gad_project/CallBuilder.java
@@ -0,0 +1,31 @@
+package com.example.gad_project;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class CallBuilder {
+
+
+    private static final String baseURL  = "https://gadsapi.herokuapp.com/api";
+
+    private static HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    private static OkHttpClient.Builder okHttp = new OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor);
+
+
+    private static Retrofit.Builder builder = new Retrofit.Builder().baseUrl(URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttp.build());
+
+    private static Retrofit retrofit = builder.build();
+
+    public static <s> s buildService(Class<s> serviceType){
+        return retrofit.create(serviceType);
+    }
+
+}
--- app/src/main/java/com/example/gad_project/MainActivity.java
+++ app/src/main/java/com/example/gad_project/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.gad_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle("LeaderBoard");
     }
 }
